9b4684fdea7ec61787f0d5b603b214b6bb1ffe05
Mauro Marcosano <mauromarcosano@gmail.com>
2024-05-21 13:30:26 -0300

    Add devServer webpack tool Add CSS and img webpack support Add clean webpack plugin Add style.css Implement npm script 'start' for devServer

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css';
+
+const app = document.querySelector('header');
+app.innerHTML = `<h1>Welcome to My Restaurant Page</h1>`;
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //Clean the dist folder before each compilation
 const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Add CSS support
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Add img support
+        type: 'asset/resource',
+      },
+    ],
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
       inject: 'body',
     }),
   ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
