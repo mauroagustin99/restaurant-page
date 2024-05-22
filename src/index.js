1123cdce00c15d5745201246cc9922144f6c6146
Mauro Marcosano <mauromarcosano@gmail.com>
2024-05-22 16:37:50 -0300

    Add logo img Add index styles

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import Home from './pages/home.js';
 import Contact from './pages/contact.js';
 import Menu from './pages/menu.js';
 import About from './pages/about.js';
+import Logo from './logo.png';
 
 //Added buttons functionality by js
 document.addEventListener('DOMContentLoaded', () => {
-  //Function to add active state to the actual loadead button
+  //Function to add active state to the actual loaded button
   function loadPage(loadFunction, buttonId) {
     loadFunction(); //Load clicked button function
     const buttons = document.querySelectorAll('.nav-btn');
@@ -17,7 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     document.getElementById(buttonId).classList.add('active'); //Add active state to the actual clicked button
   }
-  //Load home page
+
+  // Load home page after the logo is loaded
   loadPage(Home, 'home-btn');
 
   //Add event listeners to buttons
@@ -33,4 +35,11 @@ document.addEventListener('DOMContentLoaded', () => {
   document
     .getElementById('contact-btn')
     .addEventListener('click', () => loadPage(Contact, 'contact-btn'));
+
+  const logoDiv = document.querySelector('.logo-img');
+  const logo = document.createElement('img');
+  logo.src = Logo;
+  logo.alt = 'logo';
+  logo.classList.add('logo');
+  logoDiv.appendChild(logo);
 });
